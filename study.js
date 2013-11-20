fbd708327054b912012d777c9d0901d6049c25a1
Alexander Frato <alexander.frato1@marist.edu>
2013-11-20 17:57:26 -0500

    study

--- study.js
+++ study.js
@@ -12,7 +12,20 @@ function room()
 	//Hide Study
 	document.getElementById("studyDiv").style.display="none"
 }
-
+function sFriends()
+{
+	//Display story line
+	document.getElementById("outputDiv").innerHTML =
+	" What would you like to do?" +
+	"<br> You worked on homework with your friends for 4 hours." +
+	"<br><br>" + document.getElementById("outputDiv").innerHTML;
+	//Show Output
+	document.getElementById("outputDiv").style.display="block"
+	//Show Buttons
+	document.getElementById("buttonDiv").style.display="block"
+	//Hide Study
+	document.getElementById("studyDiv").style.display="none"
+}
 function sFriends()
 {
 	//Display story line
