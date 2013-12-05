eee391e141d1b2bb3225c2507be8941e35b6a939
Alexander Frato <alexander.frato1@marist.edu>
2013-12-05 13:39:41 -0500

    time still unfinished
    working out bugs with the counter

--- marist.js
+++ marist.js
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+var test = 1;
+
 //Start the game
 function createUser()
 {
@@ -35,6 +36,8 @@ function userEat()
 	document.getElementById("buttonDiv").style.display="none"
 	//Show Eat
 	document.getElementById("eatDiv").style.display="block"
+	counterA();
+	
 }
 //Send the user to STUDY
 function userStudy()
@@ -47,6 +50,8 @@ function userStudy()
 	document.getElementById("buttonDiv").style.display="none"
 	//Show Study
 	document.getElementById("studyDiv").style.display="block"
+	var a = 1;
+	counterA();
 }
 //Send the user to CLUB
 function userClub()
@@ -59,6 +64,8 @@ function userClub()
 	document.getElementById("buttonDiv").style.display="none"
 	//Show Club
 	document.getElementById("clubDiv").style.display="block"
+	var a = 1;
+	counterA();
 }
 //Send the user to CLASS
 function userClass()
@@ -71,7 +78,14 @@ function userClass()
 	document.getElementById("buttonDiv").style.display="none"
 	//Show Eat
 	document.getElementById("classDiv").style.display="block"
+	var a = 1;
+	counterA();
 }
+
+/*function dayDiv()
+{
+	document.getElementById("dayDiv").style.display=""
+}*/
 
 //Return to Menu
 function back()
@@ -86,6 +100,8 @@ function back()
 	document.getElementById("studyDiv").style.display="none"
 	//Hide Study
 	document.getElementById("clubDiv").style.display="none"
+	counterMinus(0);
+}
 //Send the user to HANG WITH FRIENDS
 function userFriends()
 {
@@ -97,323 +113,40 @@ function userFriends()
 	document.getElementById("buttonDiv").style.display="none"
 	//Show Hang with friends
 	document.getElementById("friendsDiv").style.display="block"
+	counterA();
 }
-	//Return to Menu
-function back()
+//can take part in 5 activities per day
+//after 5 activities, the next day starts
+/*function time()
 {
-	//Show Buttons
-	document.getElementById("buttonDiv").style.display="block"
-	//Hide Eat
-		document.getElementById("eatDiv").style.display="none"
-	//Show Buttons
-	document.getElementById("buttonDiv").style.display="block"
-	//Hide Study
-		document.getElementById("studyDiv").style.display="none"
-	//Show Buttons
-	document.getElementById("buttonDiv").style.display="block"
-	//Hide Class
-	document.getElementById("classDiv").style.display="none"
-}
-=======
-//Start the game
-function createUser()
-{
-	time = 0;
-	//Global Variables
-	var user = {
-	//Get the user input
-	name: document.getElementById("nameBox").value,
-	dorm: document.getElementById("dormBox").value,
-	day: document.getElementById("dayBox").value,
-}
+	//Global Variable
+	var time = 1
+	while(time>5)
+	{
+		//var x = getElementById.value(time);
+		alert("Today is Day " + time);
+		time++;
+	}
+} */
 	
-	//Display story line
-	document.getElementById("outputDiv").innerHTML =
-	"What would you like to do first?" +
-	"<br>" +
-	"Hello " + user.name + ", you have started your first day at Marist College!" +
-	" You are starting your first year in " + user.dorm + "." +
-	" You have made it through the entire semester and now you only have " + 
-	user.day + " days until finals, Good luck!";
-	//Hide Input
-	document.getElementById("inputDiv").style.display="none"
-	//Show Output
-	document.getElementById("outputDiv").style.display="block"
-	//Show Buttons
-	document.getElementById("buttonDiv").style.display="block"
-}
-
-
-//Send the user to EAT
-function userEat()
-{	
-	//Display story line
-	document.getElementById("outputDiv").innerHTML =
-	"Where would you like to go?" +
-	"<br><br>" + document.getElementById("outputDiv").innerHTML
-	//Hide Buttons
-	document.getElementById("buttonDiv").style.display="none"
-	//Show Eat
-	document.getElementById("eatDiv").style.display="block"
-}
-//Send the user to STUDY
-function userStudy()
+function day(c)
 {
-	//Display story line
-	document.getElementById("outputDiv").innerHTML =
-	"Where would you like to go?" +
-	"<br><br>" + document.getElementById("outputDiv").innerHTML
-	//Hide Buttons
-	document.getElementById("buttonDiv").style.display="none"
-	//Show Study
-	document.getElementById("studyDiv").style.display="block"
-}
-//Send the user to CLUB
-function userClub()
+	var y = 0;
+	while(y<=5)
+	{
+		c = y;
+		y++;
+	}
+} 
+
+function counterPlus(y)
 {
-	//Display story line
-	document.getElementById("outputDiv").innerHTML =
-	"Where would you like to go?" +
-	"<br><br>" + document.getElementById("outputDiv").innerHTML
-	//Hide Buttons
-	document.getElementById("buttonDiv").style.display="none"
-	//Show Club
-	document.getElementById("clubDiv").style.display="block"
-}
-//Send the user to CLASS
-function userClass()
-{
-	//Display story line
-	document.getElementById("outputDiv").innerHTML =
-	"Would you like to attend class?" +
-	"<br><br>" + document.getElementById("outputDiv").innerHTML
-	//Hide Buttons
-	document.getElementById("buttonDiv").style.display="none"
-	//Show Eat
-	document.getElementById("classDiv").style.display="block"
-}
-
-//Return to Menu
-function back()
-{
-	//Show Buttons
-	document.getElementById("buttonDiv").style.display="block"
-	//Hide Eat
-	document.getElementById("eatDiv").style.display="none"
-	//Show Buttons
-	document.getElementById("buttonDiv").style.display="block"
-	//Hide Study
-	document.getElementById("studyDiv").style.display="none"
-	//Hide Study
-	document.getElementById("clubDiv").style.display="none"
-
-}
-//Start the game
-
-function createUser()
-
-{
-
-	//Global Variables
-
-	var user = {
-
-	//Get the user input
-
-	name: document.getElementById("nameBox").value,
-
-	dorm: document.getElementById("dormBox").value,
-
-	day: document.getElementById("dayBox").value,
-
-}
-
-	//Display story line
-
-	document.getElementById("outputDiv").innerHTML =
-
-	"What would you like to do first?" +
-
-	"<br>" +
-
-	"Hello " + user.name + ", you have started your first day at Marist College!" +
-
-	" You are starting your first year in " + user.dorm + "." +
-
-	" You have made it through the entire semester and now you only have " + 
-
-	user.day + " days until finals, Good luck!";
-
-	//Hide Input
-
-	document.getElementById("inputDiv").style.display="none"
-
-	//Show Output
-
-	document.getElementById("outputDiv").style.display="block"
-
-	//Show Buttons
-
-	document.getElementById("buttonDiv").style.display="block"
-
-}
-
-//Send the user to EAT
-
-function userEat()
-
-{	
-
-	//Display story line
-
-	document.getElementById("outputDiv").innerHTML =
-
-	"Where would you like to go?" +
-
-	"<br><br>" + document.getElementById("outputDiv").innerHTML
-
-	//Hide Buttons
-
-	document.getElementById("buttonDiv").style.display="none"
-
-	//Show Eat
-
-	document.getElementById("eatDiv").style.display="block"
-
-}
-
-//Send the user to STUDY
-
-function userStudy()
-
-{
-
-	//Display story line
-
-	document.getElementById("outputDiv").innerHTML =
-
-	"Where would you like to go?" +
-
-	"<br><br>" + document.getElementById("outputDiv").innerHTML
-
-	//Hide Buttons
-
-	document.getElementById("buttonDiv").style.display="none"
-
-	//Show Study
-
-	document.getElementById("studyDiv").style.display="block"
-
-}
-
-//Send the user to CLASS
-
-function userClass()
-
-{
-
-	//Display story line
-
-	document.getElementById("outputDiv").innerHTML =
-
-	"What class would you like to attend?" +
-
-	"<br><br>" + document.getElementById("outputDiv").innerHTML
-
-	//Hide Buttons
-
-	document.getElementById("buttonDiv").style.display="none"
-
-	//Show Eat
-
-	document.getElementById("classDiv").style.display="block"
-
-}
-
-
-
-//Send the user to HANG WITH FRIENDS
-
-function userFriends()
-
-{
-
-	//Display story line
-
-	document.getElementById("outputDiv").innerHTML =
-
-	"Where would you like to go to hang out with friends?" +
-
-	"<br><br>" + document.getElementById("outputDiv").innerHTML
-
-	//Hide Buttons
-
-	document.getElementById("buttonDiv").style.display="none"
-
-	//Show Hang with friends
-
-	document.getElementById("friendsDiv").style.display="block"
-
-}
-
-
-
+	var a = y + 1;
+	y = a;
 	
-
-//Send the user to the GYM
-
-function userGym()
-
-{
-
-	//Display story line
-
-	document.getElementById("outputDiv").innerHTML =
-
-	"How long would you like to go to the gym for?" +
-
-	"<br><br>" + document.getElementById("outputDiv").innerHTML
-
-	//Hide Buttons
-
-	document.getElementById("buttonDiv").style.display="none"
-
-	//Show gym
-
-	document.getElementById("gymDiv").style.display="block"
-
-}	
-
-	//Return to Menu
-
-function back()
-
-{
-
-	//Show Buttons
-
-	document.getElementById("buttonDiv").style.display="block"
-
-	//Hide Eat
-
-		document.getElementById("eatDiv").style.display="none"
-
-	//Show Buttons
-
-	document.getElementById("buttonDiv").style.display="block"
-
-	//Hide Study
-
-		document.getElementById("studyDiv").style.display="none"
-
-	//Show Buttons
-
-	document.getElementById("buttonDiv").style.display="block"
-
-	//Hide Class
-
-	document.getElementById("classDiv").style.display="none"
-
-
 }
->>>>>>> d9ab162a3e8a9254ec185812195ab5a32298a33c
+function counterMinus(z)
+{
+	var b = z -1;
+	z = b;
+}
--- time.js
+++ time.js
@@ -8,32 +8,23 @@ function time()
 	
 	if(time < 5)
 	{
-		document.getElementById("timeDiv").innnerHTML =
-		"It is Day 1";
+		alert("It is Day 1");
 		
 	}
 	else if(time < 10)
 	{
-		document.getElementById("timeDiv").innerHTML =
-		"It is Day 2";
+		alert("It is Day 2");
 	}
-	
 	else if(time < 15)
 	{
-		document.getElementById("timeDiv").innerHTML =
-		"It is Day 3";
+		alert("It is Day 3");
 	}
-	
 	else if(time < 20)
 	{
-		document.getElementById("timeDiv").innerHTML =
-		"It is Day 4";
+		alert("It is Day 4");
 	}
-	
 	else if(time < 25)
 	{
-		document.getElementById("timeDiv").innerHTML =
-		"It is Day 5";
+		alert("It is Day 5");
 	}
-
 }	
