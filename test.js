4a354d8554ebbfef03d8ffbc5084ab2f3630c36b
nicolerae1 <nicole.rae1@marist.edu>
2013-12-13 15:55:06 -0500

    Update test.js

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ var LASTDAY = 5;
 
 function showGameStatus()
 {		
-	alert("Today is day " + day + "." + " You have done " + activities + " activities today out of 5 for a total of " + score + " points.");
+	alert("Today is day " + day + "." + " You have done " + activities + " activities today and have a total of " + score + " points out of 100.");
 	
 }
 
